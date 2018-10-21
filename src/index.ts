3741df4a5bc8ffebe224537d28f48a6ec6f74199
Tom <tom@tomrijnbeek.nl>
2018-10-21 22:43:47 +0100

    :globe_with_meridians: Implement basic hardcoded webhook message

--- src/index.ts
+++ src/index.ts
@@ -26,5 +26,18 @@ class Program {
       logger.info('Create a \'webhook.prod.json\' file to use actual settings for the bot.')
     }
     const hook = new DiscordWebhook(cfg, logger);
+
+    hook.send('', {
+            embeds: [{
+              title: 'Questholic October 2018',
+              description: 'The new magazine issue is out now!',
+              url: 'https://clanquest.org/wiki/Questaholic_-_October_2018',
+              image: {url: 'https://clanquest.org/wiki/images/thumb/7/78/October_2018_001_%28Cover%29.png/463px-October_2018_001_%28Cover%29.png'}
+            }],
+          })
+        .then(logger.info)
+        .catch(logger.error);
   }
 }
+
+Program.sendHook();
--- src/webhooks/DiscordWebhook.ts
+++ src/webhooks/DiscordWebhook.ts
@@ -1,11 +1,17 @@
-import * as Discord from 'discord.js'
+import * as Discord from 'discord.js';
 import { ILogger, IWebhookConfig } from '../api';
 
 export class DiscordWebhook {
-
   private _hook: Discord.WebhookClient;
 
   constructor(cfg: IWebhookConfig, logger: ILogger) {
     this._hook = new Discord.WebhookClient(cfg.id, cfg.token);
+  }
+  
+  send(
+      content: Discord.StringResolvable,
+      options?: Discord.WebhookMessageOptions|Discord.Attachment|Discord.RichEmbed)
+      : Promise<(Discord.Message|Array<Discord.Message>|Object|Array<Object>)> {
+    return this._hook.send(content, options);
   }
 }
