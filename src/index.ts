0abeb65ba2f65dda1c5f861f8480d763d8f12c45
Tom <tom@tomrijnbeek.nl>
2018-10-23 22:35:38 +0100

    :bug: Fix webhook prod import

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ class Program {
   public static sendHook() {
     let cfg = require('./../webhook.json') as IWebhookConfig
     try {
-      const cfgProd = require('./../webhook.prod.json') as IBotConfig
+      const cfgProd = require('./../webhook.prod.json') as IWebhookConfig
       cfg = { ...cfg, ...cfgProd }
     } catch {
       logger.info('Create a \'webhook.prod.json\' file to use actual settings for the bot.')
