9cb8ac72cb40b47deadcf6802e369bfc5c7e6bc8
Tom <tom@tomrijnbeek.nl>
2018-11-03 16:58:43 +0000

    :package: Set up ecosystem.json

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps : [{
+    name: 'QuestBot',
+    script: './dist/index.js',
+
+    args: 'runBot',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'cireon',
+      host : 'clanquest.org',
+      ref  : 'origin/master',
+      repo : 'git@github.com:tomrijnbeek/questbot.git',
+      path : '/home/cireon/questbot',
+      'post-deploy' : 'npm install && npm run-script build && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
